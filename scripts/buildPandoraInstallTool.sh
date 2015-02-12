888c3763d4abb3a254712c9bbe012c621c5fe05a
Lindsey Gray <lindsey.gray@gmail.com>
2015-02-12 15:27:18 +0100

    some skeleton code structure, bootstrap scripts

---
+++ scripts/buildPandoraInstallTool.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "$CMSSW_BASE" ]; then
+    echo "you need to run \cmsenv\""
+    exit 1
+fi  
+
+startDir=`pwd`
+cd ${CMSSW_BASE}/src
+git clone https://github.com/lgray/PandoraPFA.git ./PandoraPFA
+git clone https://github.com/lgray/PandoraSDK.git ./PandoraSDK
+git clone https://github.com/lgray/PandoraMonitoring.git ./PandoraMonitoring
+git clone https://github.com/lgray/LCContent.git ./LCContent
+export PANDORA_DIR=${CMSSW_BASE}/src/PandoraPFA
+mkdir -p $PANDORA_DIR/lib
+cd PandoraPFA/
+make -j 9
+
+cp ${CMSSW_BASE}/src/HGCal/PandoraTranslator/scripts/pandorapfanew_interface.xml ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected
+scram setup pandorapfanew_interface
+
+TEST=`scram tool list | grep pandorapfa | wc -l`
+
+if [ "$TEST" -eq "0"]
+    echo "pandora pfa was not successfullly installed :-("
+    exit 1
+fi
+
+echo "you can now link to the pandora libraries through scram!"
